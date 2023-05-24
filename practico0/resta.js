c34670c3483427395ddc624f97b02c3f83177c3c
Gabi Hak <robertohak270997@gmail.com>
2023-05-23 21:31:11 -0300

    Cree carpeta practico0

---
+++ practico0/resta.js
@@ -0,0 +1,4 @@
+let _minuendo = 40;
+let _sustraendo = 20;
+const _resultado = _minuendo - _sustraendo;
+console.log(_resultado);
--- resta.js
+++
@@ -1,3 +0,0 @@
-let _minuendo = 40;
-let _sustraendo = 20;
-console.log(_minuendo - _sustraendo);
