405c956d7bcd0b81f68f560c161edb356944d298
Gabi <robertohak270997@gmail.com>
2023-05-27 11:55:25 -0300

    nuevo archivo

---
+++ practico0/conversion.js
@@ -0,0 +1,18 @@
+// let enero = 10;
+// let febrebro = 20;
+// let marzo = 30;
+// let abril = 40;
+// let mayo = 50;
+// let junio = 60;
+// let julio = 70;
+// let agosto = 80;
+// let septiembre = 90;
+// let octubre = 100;
+// let noviembre = 110;
+// let diciembre = 120;
+// let far = (enero + febrebro + marzo + abril
+//             + mayo + junio + julio + agosto + septiembre
+//             + octubre + noviembre + diciembre)/12;
+let far = 50;
+let celsius = (far - 32) * 5/9;
+console.log(celsius);
--- practico0/resta.js
+++ practico0/resta.js
@@ -1,4 +1,8 @@
-let minuendo = 20;
+/*
+1. Pregunta
+Respuesta
+*/
+let minuendo = 30; // a. minuendo=10
 let sustraendo = 10;
 let resultado = minuendo - sustraendo;
 console.log(resultado);
